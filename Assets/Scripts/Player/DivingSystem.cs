e878d291a7edca75c0812fbd9371cf9415b9fb6e
Jacques <jacques.pillet@eisti.fr>
2018-08-08 01:02:09 +0200

    tes os

---
+++ Assets/Scripts/Player/DivingSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DivingSystem : MonoBehaviour {
+	public GameObject diving, slab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void launch() {
+		slab.GetComponent<Animator>().SetTrigger("isOpen");
+		diving.GetComponent<Animator>().SetTrigger("isLifting");
+	}
+}
