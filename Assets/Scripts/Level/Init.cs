49be75d916f24b77b9e8112907f557fb0695b0c5
Jacques <jacques.pillet@eisti.fr>
2018-08-07 17:03:27 +0200

    coffin

---
+++ Assets/Scripts/Level/Init.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Init : MonoBehaviour {
+	public static float  BOOK_SIZE = 1.05f;
+	public GameObject book;
+	public GameObject shelfs;
+
+	// Use this for initialization
+	void Start () {
+		for(int i=0; i< shelfs.transform.GetChildCount(); i++) {
+			GameObject shelf = shelfs.transform.GetChild(i).gameObject;
+			Debug.Log(shelf.transform.lossyScale);
+			// int numBooks = shelf.transform.lossyScale. / BOOK_SIZE;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
