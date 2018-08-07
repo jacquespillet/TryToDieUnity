121f500ad12a51408df738f340cf03cf10779b07
masta136 <stanislas.arnoud@protonmail.com>
2018-08-07 19:19:46 +0200

    mega immaolator

---
+++ Assets/Scripts/Objects/Immolator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Immolator : MonoBehaviour {
+
+	private int counterExplosive;
+	// Use this for initialization
+	void Start () {
+		this.counterExplosive = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.counterExplosive >= 10) {
+			Debug.Log("Ca brule memene");
+		}
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if(!other.gameObject.GetComponent<Book>().Equals(null)) {
+			this.counterExplosive++;
+			Debug.Log("FIRE IN THE HOLE!!!!!");
+		}
+	}
+}
