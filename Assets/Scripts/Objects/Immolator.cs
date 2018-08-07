3f8d6401408061b4b595c0bf06e136632c868d7c
masta136 <stanislas.arnoud@protonmail.com>
2018-08-07 22:19:18 +0200

    P0T0

--- Assets/Scripts/Objects/Immolator.cs
+++ Assets/Scripts/Objects/Immolator.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Immolator : MonoBehaviour {
 	public GameObject particles;
 	private int counterExplosive;
+	public int CONST_MAX_COUNTER; 
 	private float CONST_TIMER = 20f;
 	private float timer;
 	// Use this for initialization
 	void Start () {
 		this.counterExplosive = 0;
 		this.timer = this.CONST_TIMER;
+		this.CONST_MAX_COUNTER = 1;
 	}
 	
 	// Update is called once per frame
@@ -24,7 +26,7 @@ public class Immolator : MonoBehaviour {
 			}
 			this.timer = this.CONST_TIMER;
 		}
-		if(this.counterExplosive >= 1) {	
+		if(this.counterExplosive >= this.CONST_MAX_COUNTER) {	
 			this.transform.GetChild(1).GetComponent<Animator>().SetTrigger("isFinish");
 			this.transform.GetChild(2).GetComponent<Animator>().SetTrigger("isFinish");
 			this.counterExplosive = 0;
@@ -44,6 +46,10 @@ public class Immolator : MonoBehaviour {
 			}
 		} else if (other.gameObject.GetComponent<Controller>() != null) {
 			other.gameObject.GetComponent<Controller>().die();
+			for(int i=0; i<this.particles.transform.childCount; i++) {
+				ParticleSystem system = this.particles.transform.GetChild(i).GetComponent<ParticleSystem>();
+				system.startSize = 0;
+			}
 		}
 	}
 }
