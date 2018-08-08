7d60a246441b30979bee15419be82790f21788ad
Jacques <jacques.pillet@eisti.fr>
2018-08-08 10:09:54 +0200

    tgé

---
+++ Assets/Scripts/Objects/Painting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Painting : Item {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void beUsed() {
+		Debug.Log("c'est moi le book");
+	}
+
+}
