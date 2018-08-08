ef715a32dd405d4b8126fb5672335166877c254c
masta136 <stanislas.arnoud@protonmail.com>
2018-08-08 11:05:01 +0200

    ok

---
+++ Assets/Scripts/Objects/Floor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/// OnCollisionEnter is called when this collider/rigidbody has begun
+	/// touching another rigidbody/collider.
+	/// </summary>
+	/// <param name="other">The Collision data associated with this collision.</param>
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.gameObject.GetComponent<Controller>() != null && other.gameObject.GetComponent<Controller>().willDie){
+			other.gameObject.GetComponent<Controller>().die();
+		}
+	}
+}
--- Assets/Scripts/Player/Controller.cs
+++ Assets/Scripts/Player/Controller.cs
@@ -6,6 +6,7 @@ public class Controller : MonoBehaviour {
 	// Test commit stan
 	// Mouse and keys
 	private float x, y;
+	public bool willDie;
 	public GameObject coffin;
 	public float MAX_FORWARD_Y = 0.80f;
 	public float CONST_SPEED = 10f;
@@ -28,13 +29,13 @@ public class Controller : MonoBehaviour {
 		Cursor.lockState = wantedMode;
 		this.speed = this.CONST_SPEED;
 		this.divingSystem = this.GetComponent<DivingSystem>();
+		this.willDie = false;
 	}
 	
 	void Update () {
 		// Get directionals pushed keys and move
 		float horizontaltranslation = Input.GetAxis("Horizontal");
 		float verticalTranslation =  -Input.GetAxis("Vertical");
-		
 		this.GetComponent<Rigidbody>().velocity = new Vector3(this.transform.forward.x* verticalTranslation * this.speed, this.GetComponent<Rigidbody>().velocity.y, this.transform.forward.z* verticalTranslation* this.speed);
 		this.GetComponent<Rigidbody>().velocity += this.transform.right * horizontaltranslation * this.speed;
 		this.collider.transform.eulerAngles = new Vector3(0f, 0f, 0f);
@@ -83,13 +84,17 @@ public class Controller : MonoBehaviour {
 			this.GetComponent<Rigidbody>().velocity = new Vector3(this.GetComponent<Rigidbody>().velocity.x, this.GetComponent<Rigidbody>().velocity.y + this.CONST_JUMP, this.GetComponent<Rigidbody>().velocity.z);
 
 		}
+
+		if (this.gameObject.GetComponent<Rigidbody>().velocity.y < -5f){
+			this.willDie = true;
+		}
 	}
 
 	void checkCatchableObject() {
 		RaycastHit hit;
 		// If the raycast find an object with the tag catchatchable
 		if(Physics.Raycast(this.transform.position, this.transform.forward, out hit, 10f, 256)) {
-			Debug.Log(hit.transform.gameObject);
+			//Debug.Log(hit.transform.gameObject);
 			if(hit.transform.gameObject.tag == "Lader" && Input.GetAxis("Vertical") < 0 && hit.distance < 1f) {
 				this.transform.GetComponent<Rigidbody>().velocity = new Vector3(0f, 2.5f, 0f);
 
@@ -97,7 +102,7 @@ public class Controller : MonoBehaviour {
 			}
 			if(hit.transform.gameObject.tag == "Catchable") {
 				// Si on a pas d'objet dans les mains on le choppe poto
-				if(Input.GetKeyDown(KeyCode.E) && !this.hasObject) {
+				if(Input.GetKeyDown(KeyCode.E)) {
 					this.hasObject = true;
 					this.currentObject = hit.transform.gameObject.GetComponent<Item>();
 					this.speed = 0.75f * this.CONST_SPEED;
@@ -105,7 +110,7 @@ public class Controller : MonoBehaviour {
 				}
 			}
 			if(hit.transform.gameObject.tag == "Lever") {
-				if(Input.GetKeyDown(KeyCode.A)) {
+				if(Input.GetKeyDown(KeyCode.E)) {
 					hit.transform.gameObject.GetComponent<Animator>().SetTrigger("isPulled");
 					this.divingSystem.launch();
 				}
@@ -138,8 +143,15 @@ public class Controller : MonoBehaviour {
 	}
 
 	public void die() {
+		this.willDie = false;
 		this.numDeath++;
-		Instantiate(this.coffin, new Vector3(2.0f, 1f, 2.0f), Quaternion.identity);
+		instantiateNewCoffin();
 		this.transform.position = new Vector3(0f, 1f, 0f);
+	}
+
+	public void instantiateNewCoffin(){
+		var newCoffin = Instantiate(this.coffin, new Vector3(2.0f, 1f, 2.0f), Quaternion.identity);
+		newCoffin.transform.SetParent(this.transform.parent);
+		newCoffin.transform.localScale = new Vector3(1f,1f,1f);
 	}
 }
